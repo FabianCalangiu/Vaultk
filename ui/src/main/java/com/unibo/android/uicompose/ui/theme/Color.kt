d0dd70602d7e10c6534197ded25e1ae98784f556
Fabian Calangiu <fabian.calangiu@studio.unibo.it>
2026-05-20 15:58:26 +0200

    Add simple dark color scheme

--- ui/src/main/java/com/unibo/android/uicompose/ui/theme/Color.kt
+++ ui/src/main/java/com/unibo/android/uicompose/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.unibo.android.uicompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Background = Color(0xFF524B7B)
+val Surface = Color(0xFF2F2A52)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Header = Color(0xFF272344)
+val Accent = Color(0xFFE3C96A)
+val TextPrimary = Color(0xFFFFFFFF)
+
+val TextSecondary = Color(0xFFB7B4CC)
