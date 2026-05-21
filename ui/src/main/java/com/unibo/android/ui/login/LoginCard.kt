50aded340185ef6857ed54702a73899b4241908a
Menghetz <manuelmenghetti0@gmail.com>
2026-05-21 11:59:40 +0200

    Style: login/register

--- ui/src/main/java/com/unibo/android/ui/login/LoginCard.kt
+++ ui/src/main/java/com/unibo/android/ui/login/LoginCard.kt
@@ -1,13 +1,18 @@
 package com.unibo.android.uicompose.login
 
+import android.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,8 +21,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.unibo.android.uicompose.navigation.Routes
 
@@ -42,25 +49,31 @@ fun LoginCard(
         mutableStateOf("")
     }
 
-    Card(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(24.dp)
+            .background(MaterialTheme.colorScheme.surface)
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             Row(
-                Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+                Modifier.fillMaxWidth().padding(15.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
                 Text(
-                        text = "LOGIN"
+                        text = "LOGIN",
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
                     )
 
                 Text(
                     text = "REGISTER",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 20.sp,
                     modifier = Modifier
                         .clickable{
                             navController.navigate(Routes.FORM_REGISTER)
