bb88dd9a9145b8c064e56ac0e4a05bfd711eed36
Menghetz <manuelmenghetti0@gmail.com>
2026-05-22 11:15:41 +0200

    Add Account entry creation

--- ui/src/main/java/com/unibo/android/ui/accounts/CreateAccScreen.kt
+++ ui/src/main/java/com/unibo/android/ui/accounts/CreateAccScreen.kt
@@ -1,9 +1,15 @@
 package com.unibo.android.ui.accounts
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,8 +17,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.unibo.android.ui.vault.VaultHeader
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import com.unibo.android.ui.common.Header
+import com.unibo.android.ui.theme.Header
 
 @Composable
 fun CreateAccScreen() {
@@ -28,54 +38,89 @@ fun CreateAccScreen() {
         mutableStateOf("")
     }
 
-    Column() {
-        VaultHeader("New Account")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Header("New Account")
 
-        Box() {
-            OutlinedTextField(
-                value = accountTitle,
-                onValueChange = {
-                    accountTitle = it
-                },
-                label = {
-                    Text("Title")
-                },
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            OutlinedTextField(
-                value = emailAccount,
-                onValueChange = {
-                    emailAccount = it
-                },
-                label = {
-                    Text("Email/Username")
-                },
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(15.dp)
+                .background(Header)
+        ) {
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-            )
-
-            OutlinedTextField(
-                value = passwordAccount,
-                onValueChange = {
-                    passwordAccount = it
-                },
-                label = {
-                    Text("Password")
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-
-            Button(
-                onClick = {
-                    // Save note
-                },
-                modifier = Modifier.fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
             ) {
-                Text("Generate Password")
+
+                OutlinedTextField(
+                    value = accountTitle,
+                    onValueChange = {
+                        accountTitle = it
+                    },
+                    label = {
+                        Text("Insert title")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+
+                OutlinedTextField(
+                    value = emailAccount,
+                    onValueChange = {
+                        emailAccount = it
+                    },
+                    label = {
+                        Text("Insert email/username")
+                    },
+                    visualTransformation = PasswordVisualTransformation(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+
+                OutlinedTextField(
+                    value = passwordAccount,
+                    onValueChange = {
+                        passwordAccount = it
+                    },
+                    label = {
+                        Text("Insert password")
+                    },
+                    visualTransformation = PasswordVisualTransformation(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(
+                        onClick = {
+                            //API TO GENERATE PASSWORD
+                        },
+                        modifier = Modifier
+                            .align(Alignment.End)
+                    ) {
+                        Text("Generate Password")
+                    }
+
+                    Button(
+                        onClick = {
+                            //API TO GENERATE PASSWORD
+                        },
+                        modifier = Modifier
+                            .align(Alignment.End)
+                    ) {
+                        Text("Create Account")
+                    }
+                }
             }
         }
     }
-
 }
