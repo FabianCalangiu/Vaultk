d6eb60f4a70df79fc4f631dc13f5023017083330
Fabian Calangiu <fabian.calangiu@studio.unibo.it>
2026-05-24 11:23:40 +0200

    Removed: unused or useless dependencies, refactored code from entrypoint

--- app/src/main/java/com/unibo/android/vaultk/CustomApplication.kt
+++ app/src/main/java/com/unibo/android/vaultk/CustomApplication.kt
@@ -1,15 +1,8 @@
 package com.unibo.android.vaultk
 
 import android.app.Application
-import com.unibo.android.data.di.RepositoryProviderImpl
-import com.unibo.android.domain.di.UseCasesProvider
-
 class CustomApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-
-        UseCasesProvider.setup(
-            repositoryProvider = RepositoryProviderImpl(this)
-        )
     }
 }
--- data/src/main/java/com/unibo/android/data/remote/models/metadata/AppLanguage.kt
+++
@@ -1,7 +0,0 @@
-package com.unibo.android.data.remote.models.metadata
-
-import com.squareup.moshi.Json
-
-data class AppLanguage (
-  @Json(name = "ios" ) var ios : String? = null
-)
