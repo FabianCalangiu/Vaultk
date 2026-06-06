038819628467feb2b011db98c6355798f107ef40
Fabian Calangiu <fabian.calangiu@studio.unibo.it>
2026-06-06 11:55:13 +0200

    Add: peek icon for password field in account entry creation

--- ui/src/main/java/com/unibo/android/ui/accounts/CreateAccScreen.kt
+++ ui/src/main/java/com/unibo/android/ui/accounts/CreateAccScreen.kt
@@ -9,17 +9,26 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.node.ModifierNodeElement
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -35,6 +44,10 @@ fun CreateAccScreen(
     viewModel: CreateAccViewModel = viewModel()
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    var showPassword by remember {
+        mutableStateOf(false)
+    }
 
     val generatedPassword = navController.currentBackStackEntry
         ?.savedStateHandle
@@ -113,10 +126,38 @@ fun CreateAccScreen(
                     onValueChange = viewModel::onPasswordAccountChange,
                     label = {
                         Text("Insert password")
-                            },
-                    visualTransformation = PasswordVisualTransformation(),
-                    modifier = Modifier
-                        .fillMaxWidth()
+                    },
+
+                    visualTransformation =
+                        if (showPassword)
+                            VisualTransformation.None
+                        else
+                            PasswordVisualTransformation(),
+
+                    trailingIcon = {
+
+                        IconButton(
+                            onClick = {
+                                showPassword = !showPassword
+                            }
+                        ) {
+
+                            Icon(
+                                imageVector =
+                                    if (showPassword)
+                                        Icons.Default.VisibilityOff
+                                    else
+                                        Icons.Default.Visibility,
+                                contentDescription =
+                                    if (showPassword)
+                                        "Hide password"
+                                    else
+                                        "Show password"
+                            )
+                        }
+                    },
+
+                    modifier = Modifier.fillMaxWidth()
                 )
 
                 uiState.errorMessage?.let{ error ->
